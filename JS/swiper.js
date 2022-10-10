61232cf739aeca9383942b1f28dd5fb7e9fe21d3
Edinson Morales <89437329+Edinson25@users.noreply.github.com>
2022-10-09 20:10:07 -0500

    Add files via upload

---
+++ JS/swiper.js
@@ -0,0 +1,18 @@
+//Libreria Swiper para la animacion slide de las imagenes
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    loop:true,
+  });
---
+++ JS/typed.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed', {
+    // Waits 1000ms after typing "First"
+    strings: ['Soy Edinson Desarrollador Frontend! :)'],
+    backSpeed: 80,
+    typeSpeed:120,
+    loop: true
+  
+    });
